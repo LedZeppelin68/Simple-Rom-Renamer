fec5708a5094a932945c6cb76710a1ec051d796f
LedZeppelin68 <ledzeppelin68@gmail.com>
2019-04-16 21:39:00 +0300

    Init

--- Simple Rom Renamer/Form1.Designer.cs
+++ Simple Rom Renamer/Form1.Designer.cs
@@ -28,12 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBoxHave = new System.Windows.Forms.GroupBox();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.groupBoxMiss = new System.Windows.Forms.GroupBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBoxHave.SuspendLayout();
+            this.groupBoxMiss.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(863, 538);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(855, 512);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxHave, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxMiss, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(849, 506);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBoxHave
+            // 
+            this.groupBoxHave.Controls.Add(this.treeView2);
+            this.groupBoxHave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxHave.Location = new System.Drawing.Point(427, 273);
+            this.groupBoxHave.Name = "groupBoxHave";
+            this.groupBoxHave.Size = new System.Drawing.Size(419, 230);
+            this.groupBoxHave.TabIndex = 1;
+            this.groupBoxHave.TabStop = false;
+            this.groupBoxHave.Text = "Have";
+            // 
+            // treeView2
+            // 
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.Location = new System.Drawing.Point(3, 16);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(413, 211);
+            this.treeView2.TabIndex = 0;
+            // 
+            // groupBoxMiss
+            // 
+            this.groupBoxMiss.Controls.Add(this.treeView1);
+            this.groupBoxMiss.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxMiss.Location = new System.Drawing.Point(3, 273);
+            this.groupBoxMiss.Name = "groupBoxMiss";
+            this.groupBoxMiss.Size = new System.Drawing.Size(418, 230);
+            this.groupBoxMiss.TabIndex = 0;
+            this.groupBoxMiss.TabStop = false;
+            this.groupBoxMiss.Text = "Miss";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(3, 16);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(412, 211);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.listBox1, 2);
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(843, 135);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox1_DragOver);
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(843, 88);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(723, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(660, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(265, 55);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(544, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 2);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333333F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(843, 29);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(775, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(73, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(696, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(49, 18);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datafile";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(855, 512);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 538);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBoxHave.ResumeLayout(false);
+            this.groupBoxMiss.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBoxMiss;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBoxHave;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- Simple Rom Renamer/Form1.cs
+++ Simple Rom Renamer/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Security.Cryptography;
 
 namespace Simple_Rom_Renamer
 {
@@ -15,6 +18,280 @@ namespace Simple_Rom_Renamer
         public Form1()
         {
             InitializeComponent();
+
+            string[] datafiles = Directory.GetFiles("datafile");
+
+            for (int i = 0; i < datafiles.Length; i++)
+            {
+                datafiles[i] = Path.GetFileName(datafiles[i]);
+                comboBox1.Items.Add(datafiles[i]);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string datafile = comboBox1.Text;
+            string path_to_datafile = Path.Combine("datafile", datafile);
+
+            XmlDocument datafile_xml = new XmlDocument() { XmlResolver = null };
+            datafile_xml.Load(path_to_datafile);
+
+            XmlDocument datafile_miss_xml = new XmlDocument() { XmlResolver = null };
+            string path_to_datafile_miss = Path.Combine("miss", datafile + ".miss.xml");
+
+            if (File.Exists(path_to_datafile_miss))
+            {
+                datafile_miss_xml.Load(path_to_datafile_miss);
+            }
+            else
+            {
+                datafile_miss_xml.LoadXml("<datafile />");
+                XmlNodeList games = datafile_xml.DocumentElement.SelectNodes("game");
+
+                foreach(XmlNode game in games)
+                {
+                    XmlNode game_miss = datafile_miss_xml.ImportNode(game, true);
+                    datafile_miss_xml.DocumentElement.AppendChild(game_miss);
+                }
+
+                datafile_miss_xml.Save(path_to_datafile_miss);
+            }
+
+            XmlDocument datafile_have_xml = new XmlDocument() { XmlResolver = null };
+            string path_to_datafile_have = Path.Combine("have", datafile + ".have.xml");
+
+            if (File.Exists(path_to_datafile_have))
+            {
+                datafile_have_xml.Load(path_to_datafile_have);
+            }
+            else
+            {
+                datafile_have_xml.LoadXml("<datafile />");
+
+                datafile_have_xml.Save(path_to_datafile_have);
+            }
+
+            //XmlNodeList miss_games = datafile_miss_xml.DocumentElement.SelectNodes("game");
+
+            UpdateTree(datafile_miss_xml, treeView1, groupBoxMiss);
+            UpdateTree(datafile_have_xml, treeView2, groupBoxHave);
+
+        }
+
+        private void UpdateTree(XmlDocument datafile_miss_xml, TreeView treeView1, GroupBox groupBox1)
+        {
+            XmlNodeList miss_games = datafile_miss_xml.DocumentElement.SelectNodes("game");
+
+            long total_size = 0;
+            int total_roms = datafile_miss_xml.DocumentElement.SelectNodes("game/rom").Count;
+            int total_games = datafile_miss_xml.DocumentElement.SelectNodes("game").Count;
+
+            Invoke((MethodInvoker) delegate {
+                    treeView1.Nodes.Clear();
+                    foreach (XmlNode miss_game in miss_games)
+                    {
+                        string game_name = miss_game.Attributes["name"].Value;
+
+                        TreeNode temp_node = new TreeNode(game_name);
+
+                        XmlNodeList roms = miss_game.SelectNodes("rom");
+
+                        foreach (XmlNode rom in roms)
+                        {
+                            string rom_name = rom.Attributes["name"].Value;
+
+                            long rom_size = Convert.ToInt64(rom.Attributes["size"].Value);
+                            total_size += rom_size;
+
+                            temp_node.Nodes.Add(rom_name);
+                        }
+                        treeView1.Nodes.Add(temp_node);
+                    }
+                    treeView1.Sort();
+                    if (groupBox1.Name.Contains("Miss"))
+                    {
+                        groupBox1.Text = string.Format("Missing: {0} GiB, {1} sets, {2} roms", Math.Round(((float)total_size / 1E+9), 3), total_games, total_roms);
+                    }
+                    else
+                    {
+                        groupBox1.Text = string.Format("Have: {0} GiB, {1} sets, {2} roms", Math.Round(((float)total_size / 1E+9), 3), total_games, total_roms);
+                    }
+                });
+        }
+
+        private void listBox1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = ((string[])e.Data.GetData(DataFormats.FileDrop));
+
+            backgroundWorker1.RunWorkerAsync(files);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            XmlDocument datafile = new XmlDocument() { XmlResolver = null };
+
+            string datafile_name = string.Empty;
+            Invoke((MethodInvoker)delegate
+            {
+                datafile_name = comboBox1.Text;
+            });
+
+            if (datafile_name == string.Empty)
+            {
+                MessageBox.Show("Select Datafile");
+                return;
+            }
+
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Select Output Folder");
+                return;
+            }
+            else
+            {
+                string path_to_datafile = Path.Combine("datafile", datafile_name);
+                datafile.Load(path_to_datafile);
+            }
+
+            string[] files = (string[])e.Argument;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file_name = new FileInfo(files[i]).Name;
+
+                Invoke((MethodInvoker)delegate
+                {
+                    listBox1.Items.Add(string.Format("Processing: \"{0}\"", file_name));
+                });
+
+                string file_size = new FileInfo(files[i]).Length.ToString();
+                string file_md5 = CalculateMD5(files[i]);
+
+                Invoke((MethodInvoker)delegate
+                {
+                    listBox1.Items.Add(string.Format("MD5: \"{0}\"; Size: {1}", file_md5, file_size));
+                });
+
+                XmlNodeList roms = datafile.DocumentElement.SelectNodes(string.Format("game/rom[@md5=\"{0}\"]", file_md5));
+
+                Invoke((MethodInvoker)delegate
+                {
+                    if (roms.Count == 0)
+                    {
+                        listBox1.Items.Add("Found: 0 matches");
+                    }
+                    else
+                    {
+                        string match = (roms.Count > 1) ? "matches" : "match";
+                        listBox1.Items.Add(string.Format("Found: {0} {1}", roms.Count, match));
+                    }
+                });
+
+                for (int j = 0; j < roms.Count; j++)
+                {
+                    string old_file = files[i];
+                    string folder = roms[j].ParentNode.Attributes["name"].Value;
+                    folder = Path.Combine(textBox1.Text, folder);
+                    try { Directory.CreateDirectory(folder); }
+                    catch { };
+                    string new_file = Path.Combine(folder, roms[j].Attributes["name"].Value);
+
+                    try
+                    {
+                        if ((j + 1) == roms.Count)
+                        {
+                            File.Move(old_file, new_file);
+                        }
+                        else
+                        {
+                            File.Copy(old_file, new_file);
+                        }
+                    }
+                    catch { };
+
+                    Stats(roms[j], datafile_name);
+                }
+            }
+        }
+
+        private void Stats(XmlNode rom, string datafile_name)
+        {
+            XmlDocument miss = new XmlDocument() { XmlResolver = null };
+            XmlDocument have = new XmlDocument() { XmlResolver = null };
+
+            string path_to_datafile_miss = Path.Combine("miss", datafile_name + ".miss.xml");
+            string path_to_datafile_have = Path.Combine("have", datafile_name + ".have.xml");
+
+            miss.Load(path_to_datafile_miss);
+            have.Load(path_to_datafile_have);
+
+            XmlNode game = rom.ParentNode;
+
+            XmlNode game_to_remove_from = miss.DocumentElement.SelectSingleNode(string.Format("game[@name=\"{0}\"]", game.Attributes["name"].Value));
+
+            XmlNode rom_to_remove = miss.ImportNode(rom, true);
+            XmlNode rom_to_add = have.ImportNode(rom, true);
+
+            game_to_remove_from.RemoveChild(game_to_remove_from.SelectSingleNode(string.Format("rom[@md5=\"{0}\"]", rom_to_remove.Attributes["md5"].Value)));
+
+            if (game_to_remove_from.SelectNodes("rom").Count == 0) miss.DocumentElement.RemoveChild(game_to_remove_from);
+
+            XmlNode game_to_add = have.DocumentElement.SelectSingleNode(string.Format("game[@name=\"{0}\"]", game.Attributes["name"].Value));
+
+            if (game_to_add == null)
+            {
+                XmlElement name = have.CreateElement("game");
+                name.SetAttribute("name", game.Attributes["name"].Value);
+                name.AppendChild(rom_to_add);
+                have.DocumentElement.AppendChild(name);
+            }
+            else
+            {
+                game_to_add.AppendChild(rom_to_add);
+            }
+
+            miss.Save(path_to_datafile_miss);
+            have.Save(path_to_datafile_have);
+
+            UpdateTree(miss, treeView1, groupBoxMiss);
+            UpdateTree(have, treeView2, groupBoxHave);
+        }
+
+        private string CalculateMD5(string file)
+        {
+            MD5 hash = MD5.Create();
+
+            using (BinaryReader br = new BinaryReader(new FileStream(file, FileMode.Open)))
+            {
+                long file_size = br.BaseStream.Length;
+
+                while (file_size > 0)
+                {
+                    int block_size = ((file_size % 2048) == 0) ? 2048 : (int)(file_size % 2048);
+
+                    byte[] temp_block = br.ReadBytes(block_size);
+
+                    hash.TransformBlock(temp_block, 0, block_size, null, 0);
+
+                    file_size -= block_size;
+                }
+
+                hash.TransformFinalBlock(new byte[0], 0, 0);
+            }
+            string md5 = BitConverter.ToString(hash.Hash).Replace("-", "").ToLower();
+            hash.Dispose();
+            return md5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
         }
     }
 }
